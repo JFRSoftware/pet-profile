448c18ab5ec14220331aee837bc0db402c7e65e5
felipe.ro <felipe.ro@compasso.com.br>
2023-04-19 21:06:48 -0300

    Project started

---
+++ src/main/java/com/example/petprofile/PetProfileApplication.java
@@ -0,0 +1,13 @@
+package com.example.petprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petprofile/PetProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
